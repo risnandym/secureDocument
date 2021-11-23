e32bde7cacd693bee50b1a2afd4de3bb3c7e94aa
risnandym <risnandy.maulana@gmail.com>
2021-11-23 17:08:39 +0700

    add QRC and PDF to db

--- app/Http/Controllers/FileUpload.php
+++ app/Http/Controllers/FileUpload.php
@@ -38,26 +38,32 @@ class FileUpload extends Controller
         
             $pdf = new FPDI();
 
-        $pagecount = $pdf->setSourceFile(Storage::path($filePath));
-        for ($pageNo = 1; $pageNo <= $pagecount; $pageNo++) {
-            // import a page
-            $templateId = $pdf->importPage($pageNo);
-            // get the size of the imported page
-            $size = $pdf->getTemplateSize($templateId);
-            $pdf->addPage();
-            // use the imported page
-            $pdf->useTemplate($templateId);
-        }
-        
-        $pdf->Image(Storage::path('basic/QRC.png'), 175, 262, 30,30);
-        $dir = "../storage/app/public/";
-        // $pdf->Output($dir.$fileName, 'F');
- 
-        
+            $pagecount = $pdf->setSourceFile(Storage::path($filePath));
+            for ($pageNo = 1; $pageNo <= $pagecount; $pageNo++) {
+                // import a page
+                $templateId = $pdf->importPage($pageNo);
+                // get the size of the imported page
+                $size = $pdf->getTemplateSize($templateId);
+                $pdf->addPage();
+                // use the imported page
+                $pdf->useTemplate($templateId);
+            }
+            // input QRC to lastpage
+            $pdf->Image(Storage::path('basic/QRC.png'), 175, 262, 30,30);
+            
+            // output and store to database
+            $dir = "../storage/app/public/";
             $fileModel->name = time().'_'.$req->file->getClientOriginalName();
             $fileModel->file_path = '/storage/' . $pdf->Output($dir.$fileName, 'F');
             $fileModel->save();
-            return view('home');
+
+            // $digest = hash_file('sha3-512', $dir.$fileName, true);
+            $digest = hash_hmac_file('sha3-512', $dir.$fileName, $rd_string, true);
+            // return $digest;
+            // $getteks = file_get_contents($dir.$fileName, true);
+            // $teks = file_put_contents('hashed.txt', $getteks);
+            // return $teks;
+            readfile($dir.$fileName, true);
         }
     }
 }
